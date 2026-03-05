4726141b26bdd55c7b44829b60777752211bc322
j-ai-616 <yjson616@gmail.com>
2026-03-05 11:54:11 +0900

    Feat: 모듈, 패키지, 파일 IO 기본 완료

---
+++ 07_io/main.py
@@ -0,0 +1,7 @@
+from text_basic import demo_open_close_and_modes, demo_with_is_recommended
+
+if __name__ == "__main__":
+    demo_open_close_and_modes()
+    print('-' * 60)
+    
+    demo_with_is_recommended()
